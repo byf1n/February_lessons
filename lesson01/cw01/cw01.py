55fa8c17aca27e76e44ad15a7c68b3c3487a9e43
byf1n <kirillozubenko@gmail.com>
2022-02-16 23:57:57 +0200

    чуть доделал

--- lesson01/cw01/cw01.py
+++ lesson01/cw01/cw01.py
@@ -14,28 +14,35 @@
 # приклад юзера {'id':1,'name':'Max', 'age':35,'status':False}
 
 users_list = []
-def prints():
+con = True
+while con == True:
     print('1) додававання нового юзера')
     print('2) вивід всіх юзерів')
     print('3) вивід всіх юзерів за віком')
     print('4) видалення юзера по id')
     print('5) заміна статуса юзера на протилежний')
     print('6) вихід з меню')
-prints()
-num = int(input('what num:'))
-if num == 1:
-    id = int(input('id: '))
-    name = input('name: ')
-    age = int(input('age: '))
-    status = bool(input('status: '))
-    new_user = {f'id':{id},'name':{name},'age':{age},'status':{status}}
-    users_list.append(new_user)
-    prints()
     num = int(input('what num:'))
-if num == 2:
-    print(2)
-
-
+    if num == 1:
+        id = int(input('id: '))
+        name = input('name: ')
+        age = int(input('age: '))
+        status = bool(input('status: '))
+        new_user = {f'id':{id},'name':{name},'age':{age},'status':{status}}
+        users_list.append(new_user)
+    elif num == 2:
+        print(users_list)
+    elif num == 3:
+        print('в разроботке...')
+    elif num == 4:
+        delUser = int(input('which user: '))
+        print(f'delete user {delUser}')
+    elif num == 5:
+        print('change status')
+    elif num == 6:
+        con = False
+    else:
+        print('erorro!!!')
 
 
 
